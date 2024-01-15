a2e8a4ec9b6b4d65b46c4e66c6fdb052cc45827c
Himanshu Singh Chauhan <hsc92180@gmail.com>
2024-01-15 21:43:16 +0530

    Keyboard code

---
+++ KeyboardCode/README.md
@@ -0,0 +1,5 @@
+# Keyboard Code: 
+
+## [Hosted Link!](https://hsc92180.github.io/JS-Projects/KeyboardCode/)
+
+![Alt text](image.png)
--- KeyboardCode/script.js
+++ KeyboardCode/script.js
@@ -1,19 +1,56 @@
-function keyboardCode(){
+const statement = document.getElementById("statement");
+const KEY = document.getElementById("key");
+const KEYCODE = document.getElementById("keycode");
 
+let keysPressed = {};
+document.addEventListener("keydown", (e) => {
+  keysPressed[e.key] = true;
+  statement.innerHTML = "You Pressed &nbsp;";
+  if (keysPressed["Control"] && e.key == "z") {
+    // alert(event.key);
+    document.getElementById("audio").play();
 
-document.addEventListener('keydown', (e)=>{
-    console.log(e);
-    
-    // Update Key Name in DOM
-    // const keyName = document.getElementById("keyName");
-    const keyName = document.querySelector("div > h1 > span");
-    
-    keyName.innerText = e.key;
-    //console.log(e.key);
+    KEY.innerHTML = `Control + z`;
+    KEYCODE.innerText = "🔊";
+    return;
+  } else if (keysPressed["Control"] && e.key == "c") {
+    document.getElementById("audio1").play();
 
-    // Update KeyNo
-    const keyNumber = document.querySelector("#keyNumber");
-    keyNumber.innerText = e.keyCode;
-});}
+    KEY.innerHTML = `Control + c`;
+    KEYCODE.innerText = "🔊";
+    return;
+  } else if (keysPressed["Control"] && e.key == "x") {
+    document.getElementById("audio2").play();
 
-keyboardCode();
+    KEY.innerHTML = `Control + x`;
+    KEYCODE.innerText = "🔊";
+    return;
+  } else if (keysPressed["Control"] && e.key == "v") {
+    document.getElementById("audio3").play();
+
+    KEY.innerHTML = `Control + v`;
+    KEYCODE.innerText = "🔊";
+    return;
+  } else if (keysPressed["Control"] && e.key == "d") {
+    document.getElementById("audio4").play();
+
+    KEY.innerHTML = `Control + d`;
+    KEYCODE.innerText = "🔊";
+    return;
+  }
+
+  const keyCode = e.keyCode || e.which; //handle different browser
+  let key = e.key;
+
+  if (key == " ") {
+    KEY.innerHTML = "Space";
+  } else {
+    KEY.innerHTML = key;
+  }
+
+  KEYCODE.innerText = e.keyCode;
+});
+
+document.addEventListener("keyup", (event) => {
+  delete keysPressed[event.key];
+});
