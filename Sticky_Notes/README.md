613d6a711d1eb768d43b2fda1ea1accb44a79e6b
Himanshu Singh Chauhan <hsc92180@gmail.com>
2024-01-16 23:53:27 +0530

    sticky notes

---
+++ Sticky_Notes/README.md
@@ -0,0 +1,10 @@
+# Sticky-Notes
+
+Welcome to Sticky Notes! This application allows you to create,
+manage and organize your notes easily and efficiently. You can add new notes in the notes section and also remove the added notes from that section 
+
+
+## [Hosted Link!](https://hsc92180.github.io/JS-Projects/Sticky_Notes/)
+
+
+
--- Sticky_Notes/script.js
+++ Sticky_Notes/script.js
@@ -22,26 +22,32 @@ btn.addEventListener("click", () => {
 });
 
 function removeStickyNotes(){
-    // btn2 = document.querySelectorAll(".btn2");
-    // btn2.forEach((singlrBtn) => {
-    //     singlrBtn.addEventListener("click", (e) => {
-    //         e.target.parentElement.remove();
-    //     });
-    // });
-
-    let rc = document.querySelectorAll(".rightContainer");
-
-    rc.addEventListener("click", (e) => {
-        if(e.target.innerText === 'X'){
+    btn2 = document.querySelectorAll(".btn2");
+    btn2.forEach((singlrBtn) => {
+        singlrBtn.addEventListener("click", (e) => {
             e.target.parentElement.remove();
-        }
+        });
     });
+
+    // let rc = document.querySelectorAll(".rightContainer");
+
+    // rc.addEventListener("click", (e) => {
+    //     if(e.target.innerText === 'X'){
+    //         e.target.parentElement.remove();
+    //     }
+    // });
 };
 
 function makeStickyNotes(text, color){
     let div = document.createElement("div");
-    div.innerHTML = `<p>${text}</p><button class="btn2">X</button`;
+    div.innerHTML = `<p style="width: 98%; color: white; font-weight: medium;">${text}</p><button class="btn2" style=" background-color: red; color: white;">X</button`;
     div.style.backgroundColor = color;
+    div.style.margin = '20px';
+    div.style.padding = '5px';
+    // div.style.width = '90%';
+    div.style.borderRadius = '5px'
+    div.style.boxShadow = '5px 5px 10px rgba(0, 0, 0, 0.5)'
+    div.style.display = 'flex'
     rightContainer.appendChild(div);
 
 };
