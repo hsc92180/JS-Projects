de0f9ee440d3357b8bf76e4cc6349c04941b7379
Himanshu Singh Chauhan <hsc92180@gmail.com>
2024-01-17 22:57:10 +0530

    emoji Project

---
+++ EmojiProject/README.md
@@ -0,0 +1,8 @@
+# Emoji-Project
+
+Welcome to Emoji! This application allows you to see all the emojees, search particular type of emojee 
+
+## [Hosted Link!](https://hsc92180.github.io/JS-Projects/EmojiProject/)
+
+
+https://github.com/hsc92180/Geekster_Projects/assets/68774484/85a125a0-1ac6-4bc6-b95e-9dfb04b51f08
--- EmojiProject/script.js
+++ EmojiProject/script.js
@@ -1,43 +1,57 @@
-function searchEmoji(){
-    let inputValue = document.getElementById("search_field").value;
-    displayResult(inputValue);
+const searchEmoji = e=>{
+    e.preventDefault();
+    const value = document.getElementById("serach_field").value;
+    console.log(value);
+    displaySearchResults(value);
     return false;
 }
 
-function displayResult(searchQuery=""){
-    let filteredData = emojiList.filter((e) => {
-        if((e.description.indexOf(searchQuery) != -1) 
-        || (e.tags.some(elem => elem.startsWith(searchQuery))) 
-        || (e.tags.some(elem => elem.startsWith(searchQuery)))){
+const autoSearch = e=>{
+    const value = e.target.value;
+    console.log(value);
+    displaySearchResults(value);
+}
+
+const displaySearchResults = (searchQuery = "") =>{
+    const filtered = emojiList.filter(e=>{
+        if(e.description.indexOf(searchQuery) != -1){
             return true;
         }
+        if(e.aliases.some(elem=>elem.startsWith(searchQuery))){
+            return true;
+        }
+        if(e.tags.some(elem=>elem.startsWith(searchQuery))){
+            return true;
+        }
+    });
 
-    })
-
-    let parent = document.getElementById("search_result_container");
-    console.log(parent);
+    const parent = document.getElementById("search_result_container");
     parent.innerHTML = "";
+    filtered.forEach(e=>{
+        const new_row = document.createElement('tr');
+        const new_emoji = document.createElement('td');
+        const new_aliases = document.createElement('td');
+        const new_desc = document.createElement('td');
 
-    filteredData.forEach((e) => {
-        let new_row = document.createElement("tr");
-        let new_emoji = document.createElement("td");
-        let new_aliases = document.createElement("td");
-        let new_desc = document.createElement("td");
-        
-        // new_emoji.innerText = e.emoji;
-        // new_aliases.innerText = e.aliases;
-        // new_desc.innerText = e.description;
+        new_emoji.innerText = e.emoji;
+        new_aliases.innerText = e.aliases.join(", ");
+        new_desc.innerText = e.description;
 
-        //console.log(new_emoji, new_aliases, new_desc);
+        new_aliases.innerText = new_aliases.innerText.replaceAll('_', " ");
+
+
+        new_emoji.classList.add("emoji");
+        new_aliases.classList.add("aliases");
+        new_desc.classList.add("desc");
 
         new_row.appendChild(new_emoji);
         new_row.appendChild(new_aliases);
-        new_row.append(new_desc);
-        
+        new_row.appendChild(new_desc);
         parent.appendChild(new_row);
     })
 }
 
-console.log(emojiList);
-document.getElementById("search_field").addEventListener("keyup", searchEmoji());
-window.onload = () => displayResult();
+
+document.getElementById("search_form").addEventListener('submit',searchEmoji);
+document.getElementById("search_field").addEventListener("keyup", autoSearch)
+window.onload = ()=> displaySearchResults();
